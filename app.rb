c556f6b719db58fb79b07811a87a4de25da55a61
Mihser <mihsee436@gmail.com>
2025-06-06 19:49:50 +0800

    Add /new and new.erb

--- app.rb
+++ app.rb
@@ -6,37 +6,6 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
-get '/about' do  
-	erb :about
-end 
-
-get '/visit' do  
-	erb :visit
-end 
-get '/contacts' do  
-	erb :contacts
-end 
-
-post '/visit' do 
-	@user_name = params[:username]
-	@phone = params[:phone]
-	@date_time = params[:datetime]
-	@hairdresser = params[:hairdresser]
-
-	f = File.open('./public/users.txt', 'a')
-	f.write("User name:#{@user_name}, Phone:#{@phone}, Date and time:#{@date_time}, Hairdresser:#{@hairdresser}\n")
-	f.close
-
-	erb :visit
-end	 
-
-post '/contacts' do 
-	@email = params[:email]
-	@message = params[:message]
-
-	f1 = File.open('./public/contacts.txt', 'a' )
-	f1.write("Email:#{@email}, Message:#{@message}")
-	f1.close
-
-	erb :contacts
-end	
+get '/new' do
+  erb :new
+end
