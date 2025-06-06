092271ebe97e628eb8807242aa89dc45f4048363
Mihser <mihsee436@gmail.com>
2025-06-07 00:19:25 +0800

    Add author ,  validation for /post and new(homework)

--- app.rb
+++ app.rb
@@ -22,6 +22,7 @@ configure do
 # создаёт таблицу , если ее нет, при каждом запуски программы 	
 	@db.execute 'CREATE TABLE IF NOT EXISTS Posts (
 	ID	INTEGER,
+	author TEXT,
 	created_date DATA,
 	content	TEXT,
 	PRIMARY KEY("ID" AUTOINCREMENT))'
@@ -51,16 +52,26 @@ end
 post '/new' do
   
   	content = params[:content]
+  	author = params[:autor]
+	
+	# сохранение данных в БД
+	@db.execute 'insert into Posts (autor, content, created_date) values (?, ?, datetime ())', [author, content]
 
 	# проверка на пустое поле
+	# hh = {:author => 'Type autorname', :content => 'Type text'}
+	
+	# @error = hh.select {|k,v| params[k] == ""}.values.join(", ")
+	
+  	if author.length <= 0 
+  		@error = 'Type Autorname'
+  		return erb :new
+	end
+
   	if content.length <= 0 
   		@error = 'Type text'
   		return erb :new
 	end
 	
-	# сохранение данных в БД
-	@db.execute 'insert into Posts (content, created_date) values (?, datetime ())', [content]
-
 	redirect to '/'
 end
 
@@ -71,13 +82,18 @@ get '/post/:post_id' do
 	@row = @results[0]
 
 	@comments = @db.execute 'select * from Comments where post_id = ? order by id',[post_id]
-	@com = @comments[0]
+
 	erb :post
 end
 
 post '/post/:post_id' do
 	post_id = params[:post_id]
 	content = params[:content]	
+	
+	# if content.length <= 0 
+  	#  	@error = 'Type comment'
+  	#  	return /post/post_id
+	#  end
 	
 	@db.execute 'insert into Comments 
 	(content, created_date, post_id) 
