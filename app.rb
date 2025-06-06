ba0e407bbc9da95e2a0a0395e32cef2f02febb71
Mihser <mihsee436@gmail.com>
2025-06-06 21:03:43 +0800

    Create table 'Posts'

--- app.rb
+++ app.rb
@@ -5,11 +5,23 @@ require 'sqlite3'
 
 def init_db
 	@db = SQLite3::Database.new 'Leprosorium.db'
+	@db.results_as_hash = true
 end	
 
 before do 
 	init_db
 end	
+
+configure do 
+	
+	init_db
+	
+	@db.execute 'CREATE TABLE IF NOT EXISTS Posts (
+	ID	INTEGER,
+	created_date	DATA,
+	content	TEXT,
+	PRIMARY KEY("ID" AUTOINCREMENT))'
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
